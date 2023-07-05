358bb7a5d923983bcac4f754320673383e602238
Noel Welsh <noel@noelwelsh.com>
2023-07-05 13:05:33 +0100

    Improve animation
    - Frame is centered on origin (required in newer Doodle)
    - Orbit is circular and used polar coordinates for clarity

--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -1,10 +1,10 @@
-import doodle.core._
-import doodle.image._
-import doodle.image.syntax.all._
-import doodle.image.syntax.core._
-import doodle.java2d._
-import doodle.reactor._
-import scala.concurrent.duration._
+import doodle.core.*
+import doodle.image.*
+import doodle.image.syntax.all.*
+import doodle.image.syntax.core.*
+import doodle.java2d.*
+import doodle.reactor.*
+import scala.concurrent.duration.*
 import cats.effect.unsafe.implicits.global
 
 // To use this example:
@@ -21,25 +21,25 @@ object Example {
 
   val animation =
     Reactor
-      .init(-200)
-      .withOnTick(x => x + 1)
-      .withStop(x => x > 200)
+      .init(0.degrees)
+      .withOnTick(a => a + 1.degrees)
+      .withStop(a => a > 360.degrees)
       .withTickRate(20.millis)
-      .withRender { x =>
-        val y = x.degrees.sin * 200
-        val planet = Image.circle(20.0).noStroke.fillColor(Color.seaGreen)
+      .withRender { a =>
+        val location = Point(200, a)
+        val planet = Image.circle(40.0).noStroke.fillColor(Color.seaGreen)
         val moon = Image
-          .circle(5.0)
+          .circle(10.0)
           .noStroke
           .fillColor(Color.slateGray)
-          .at((x * 10).degrees.cos * 50, (x * 10).degrees.sin * 50)
+          .at(Point(60, a * 5))
 
-        moon.on(planet).at(x, y)
+        moon.on(planet).at(location)
       }
 
-  val frame = Frame.size(600, 600)
+  val frame = Frame.default.withSize(600, 600).withCenterAtOrigin
 
-  def main(args: Array[String]): Unit = {
+  @main def go(): Unit = {
     image.draw()
 
     // Comment out the above and uncomment the below to display the animation
