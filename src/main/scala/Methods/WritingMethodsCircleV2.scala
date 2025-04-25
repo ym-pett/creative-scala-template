213240eb519b2a88d5bd640991d9c009da9e20a3
ym-pett <ympettinato@gmail.com>
2025-04-25 17:04:03 +0100

    adding version with for loop

---
+++ src/main/scala/Methods/WritingMethodsCircleV2.scala
@@ -0,0 +1,28 @@
+import cats.effect.unsafe.implicits.global
+import doodle.core.*
+import doodle.image.*
+import doodle.syntax.all.*
+import doodle.image.syntax.all.*
+import doodle.java2d.*
+
+@main def WritingMethodsForloop(): Unit = {
+
+    def circle(color: Color, n: Int): Image = 
+      Image.circle(50 + (n * 10)).
+      strokeColor(color.spin((15 * n).degrees)).
+      strokeWidth(3)
+
+    // TODO: talk over this
+    // TODO FUTURE: when I know more ways of looping, rewrite with different operations? 
+    def concentricCircles(color: Color): Image = {
+      val circles = (0 to 4).map { n =>
+      circle(color, n)
+      }
+      circles.reduceLeft((a, b) => a.on(b))
+    }
+
+
+    concentricCircles(Color.cadetBlue).draw()
+
+
+}
