3919241f21b1623d445a2bf7b4f32f7ee463c833
ym-pett <ympettinato@gmail.com>
2025-06-09 14:53:03 +0100

    polygons with error

--- src/main/scala/Points/Polygons.scala
+++ src/main/scala/Points/Polygons.scala
@@ -9,23 +9,26 @@ import doodle.java2d.*
 
 @main def polyP(): Unit = {
 
-  def polygonPoints(sides: Int, radius: Int): Image = {
+  def polygonPoints(sides: Int, radius: Double): Image = {
 
     val inc = 360 / sides
 
     val dot = Image.circle(5)
 
     def loop(count: Int): Image = {
-      
+      // TODO: discuss why this doesn't work, I'd like to do n += inc on each loop  
       count match {
         case 0 => Image.empty
-        case n => dot.on(loop(dot.at(Point(radius, (n * inc).degrees))))
+        case n => dot.at(Point(radius, n +inc)).on(loop(n - 1))
       }
     }
 
-    loop(count)
+    loop(sides)
   }
 
-  polygonPoints(3, 10)
+  polygonPoints(3, 50)
+  .fillColor(Color.crimson)
+  .beside(polygonPoints(5, 50).fillColor(Color.lawngreen))
+  .beside(polygonPoints(7, 50).fillColor(Color.dodgerBlue)).draw()
 
 }
