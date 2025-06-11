6696351a3c3a7825385e94d300631b5ec1ba72f7
ym-pett <ympettinato@gmail.com>
2025-06-11 19:47:36 +0100

    fixed polygons

--- src/main/scala/Points/Polygons.scala
+++ src/main/scala/Points/Polygons.scala
@@ -6,20 +6,22 @@ import doodle.image.*
 import doodle.syntax.all.*
 import doodle.image.syntax.all.*
 import doodle.java2d.*
+import scala.annotation.tailrec
 
 @main def polyP(): Unit = {
 
+  // can move this outside as I only need to define once
+  val dot = Image.circle(5)
+
   def polygonPoints(sides: Int, radius: Double): Image = {
 
-    val inc = 360 / sides
-
-    val dot = Image.circle(5)
-
+    val inc = 360 / sides // 120
+// TODO: stretch goal make it tail recursive :)
     def loop(count: Int): Image = {
-      // TODO: discuss why this doesn't work, I'd like to do n += inc on each loop  
+
       count match {
         case 0 => Image.empty
-        case n => dot.at(Point(radius, n +inc)).on(loop(n - 1))
+        case n => dot.at(Point(radius, (n *inc).degrees)).on(loop(n - 1))
       }
     }
 
