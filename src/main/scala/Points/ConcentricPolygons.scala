b2c90f1fbd4314c94f5e35526ed23d619ecfba11
ym-pett <ympettinato@gmail.com>
2025-06-19 15:48:21 +0100

    concentricpoly wip

---
+++ src/main/scala/Points/ConcentricPolygons.scala
@@ -0,0 +1,34 @@
+package points
+
+import cats.effect.unsafe.implicits.global
+import doodle.core.*
+import doodle.image.*
+import doodle.syntax.all.*
+import doodle.image.syntax.all.*
+import doodle.java2d.*
+
+@main def concentricP(): Unit = {
+// TODO: could i have imported this function from Paths.scala? 
+  def regularPolygon(sides: Int, radius: Double): Image = {
+
+    val inc = 360 / sides
+
+    def loop(count: Int): ClosedPath = {
+
+      count match {
+        case 0 => ClosedPath.empty.moveTo(radius, 0.degrees)
+        case n => loop(n-1).lineTo(radius, (n*inc).degrees)
+      }
+    }
+    Image.path(loop(sides))
+  }
+
+  def stackingPolygons(count: Int): Image = {
+
+    def loop(count: Int) : Image = {
+      case 0 => Image.empty // or should this be when sides = 3? but then we'd be going out, n +1, rather than in, n-1!
+      case n => regularPolygon().on(regularPolygon(loop(n-1))
+
+    }
+  }
+}
