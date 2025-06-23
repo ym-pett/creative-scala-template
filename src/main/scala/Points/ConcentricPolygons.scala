a766758c1cab37e53bd3dd9d766b2a31c0daf7fb
ym-pett <ympettinato@gmail.com>
2025-06-23 17:06:22 +0100

    concentric improved

--- src/main/scala/Points/ConcentricPolygons.scala
+++ src/main/scala/Points/ConcentricPolygons.scala
@@ -23,16 +23,19 @@ import doodle.java2d.*
     Image.path(loop(sides))
   }
 
-  def stackingPolygons(count: Int, first_radius: Double): Image = {
+  def stackingPolygons(count: Int, biggest_radius: Double): Image = {
+
+    // figure out what the decrement should be
+    val dec = biggest_radius / count
 
     count match {
       case 0 =>
         Image.empty // or should this be when sides = 3? but then we'd be going out, n +1, rather than in, n-1!
-      case n => stackingPolygons(n - 1, first_radius * 1.5).on(regularPolygon(n, first_radius))
+      case n => stackingPolygons(n - 1, biggest_radius - dec).on(regularPolygon(n, biggest_radius))
       // case n => regularPolygon(loop(n-1), first_radius * 1.5).on(regularPolygon(n, first_radius)
     }
   }
 
-  stackingPolygons(6, 10).draw()
+  stackingPolygons(6, 50).draw()
 
 }
