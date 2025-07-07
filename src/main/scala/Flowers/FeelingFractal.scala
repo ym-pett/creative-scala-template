09a2328dc80d100691f9bb2fe3fd81ba3ce1461b
ym-pett <ympettinato@gmail.com>
2025-07-07 17:36:12 +0100

    more sierpinski

--- src/main/scala/Flowers/FeelingFractal.scala
+++ src/main/scala/Flowers/FeelingFractal.scala
@@ -20,6 +20,21 @@ import doodle.java2d._
       case n => sierpinski(n, fold(count - 1, base, sierpinski))
     }
 
-  fold(5, basicUnit, sierpinski).draw()
+  // fold(5, basicUnit, sierpinski).draw()
+
+  ////////////////// Once more to get the hang //////////////////
+
+  val basicCircle = Image.circle(5).strokeColor(Color.turquoise)
+
+  val serbubble:(Int, Image) => Image = 
+    (count, image) => image.above(image.strokeColor(Color.turquoise.spin(10.degrees * count)).beside(image.strokeColor(Color.turquoise.spin(15.degrees * count))))
+
+  def foldBubble(count: Int, base: Image, build: (Int, Image) => Image): Image =
+    count match{
+      case 0 => base
+      case n => build(n, foldBubble(count-1, base, build))
+    }
+
+  foldBubble(7, basicCircle, serbubble).draw()
 
 }
